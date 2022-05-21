0d44a1f85d3919aa017a5df47708ce7bd6557073
ivanleongwm <ivanleongwm@gmail.com>
2022-05-21 17:09:20 +0800

    login form added

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,28 +1,48 @@
 import React from "react";
 import { Form , Button, Row , Col} from "react-bootstrap"
+import { useState, useEffect } from "react";
+
+const handleLogin = (username,password) => {
+    fetch("/api/users/login", { 
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+          username: username, 
+          password: password})
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
+  }
 
 function LoginForm() {
-    return (
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+    const [username, setUsername] = useState("Michael");
+    const [password, setPassword] = useState("123456");
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+    return (
+        <>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" onChange={(event) => setUsername(event.target.value)}/>
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit" onClick={() => {handleLogin(username,password)}}>
+                    Submit
+                </Button>
+            </Form>
+        </>
     )
 }
 
