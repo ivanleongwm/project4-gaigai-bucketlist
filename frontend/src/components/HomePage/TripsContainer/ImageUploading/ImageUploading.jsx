19d3317772009c58708196df8c95a3beaec41ae0
ivanleongwm <ivanleongwm@gmail.com>
2022-05-29 16:36:04 +0800

    image upload button added

--- frontend/src/components/HomePage/TripsContainer/ImageUploading/ImageUploading.jsx
+++ frontend/src/components/HomePage/TripsContainer/ImageUploading/ImageUploading.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import {useRef} from 'react';
 
 function UploadImages() {
+    const inputRef = useRef(null);
+
+    const handleUpload = () => {
+        inputRef.current?.click();
+    };
+
     return (
         <div className="m-3">
-          <label className="mx-3">Choose file: </label>
-          <input className="d-none" type="file" />
-          <button className="btn btn-outline-primary">Upload</button>
+        <label className="mx-3">Choose file: </label>
+        <input ref={inputRef} className="d-none" type="file" />
+        <button onClick={handleUpload} className="btn btn-outline-primary">
+            Upload
+        </button>
         </div>
-      );
+    );
 }
 
 export default UploadImages;
