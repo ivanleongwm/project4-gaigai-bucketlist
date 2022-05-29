57ae8431b680b1015e603c6374cb8747d9f8fa7d
ivanleongwm <ivanleongwm@gmail.com>
2022-05-30 03:10:08 +0800

    refactored modal popup on homepage for creating new trip

--- frontend/src/components/HomePage/CreateNewTripModal/CreateNewTripModal.jsx
+++ frontend/src/components/HomePage/CreateNewTripModal/CreateNewTripModal.jsx
@@ -1,4 +1,5 @@
-import {Modal, Button} from 'react-bootstrap'
+import './CreateNewTripModal.css'
+import {Modal, Button, Form, Row, Col} from 'react-bootstrap'
 
 function MyVerticallyCenteredModal(props) {
     return (
@@ -10,16 +11,52 @@ function MyVerticallyCenteredModal(props) {
       >
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
+            Create New Trip
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </p>
+        <Form>
+            <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridStartDate">
+                <Form.Label>Start Date</Form.Label>
+                <Form.Control type="date" placeholder="Pick Start Date" />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridEndDate">
+                <Form.Label>End Date</Form.Label>
+                <Form.Control type="date" placeholder="Pick End Date" />
+                </Form.Group>
+            </Row>
+
+            <Form.Group className="mb-3" controlId="formGridLocation">
+                <Form.Label>Location</Form.Label>
+                <Form.Select defaultValue="Choose...">
+                    <option>Choose...</option>
+                    <option>...</option>
+                </Form.Select>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formGridActivityTitle">
+                <Form.Label>Activity Title</Form.Label>
+                <Form.Control placeholder="Brief title of trip" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Brief Description of To Dos</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridCity">
+                <Form.Label>Thumbnail Url</Form.Label>
+                <Form.Control/>
+                </Form.Group>
+                <Form.Group as={Col} className="mb-3" id="formGridCheckbox">
+                <Form.Check type="switch" label="Public Post" />
+                </Form.Group>
+            </Row>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={props.onHide}>Close</Button>
--- models/Trip.js
+++ models/Trip.js
@@ -3,10 +3,13 @@ const Schema = mongoose.Schema;
 
 const tripSchema = new Schema({
     tripIndex: {type: Number, required: false},
+    username: {type: String, required: false},
     location: {type: String, required: false},
     startDate: {type: Date, required: false},
     endDate: {type: Date, required: false},
-    activity: {type: String, required: false},
+    title: {type: String, required: false},
+    thumbnail: {type: String, required: false},
+    description: {type: String, required: false},
     photos: [String],
     public: {type: Boolean, required: false}
 });
