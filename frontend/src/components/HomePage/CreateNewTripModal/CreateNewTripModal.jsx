114e93af66f1a4a4d275930effdf35b7b5bbd11a
ivanleongwm <ivanleongwm@gmail.com>
2022-05-30 04:11:40 +0800

    added handle change function to create new trip popup, to store input data in state

--- frontend/src/components/HomePage/CreateNewTripModal/CreateNewTripModal.jsx
+++ frontend/src/components/HomePage/CreateNewTripModal/CreateNewTripModal.jsx
@@ -1,7 +1,27 @@
 import './CreateNewTripModal.css'
 import {Modal, Button, Form, Row, Col} from 'react-bootstrap'
+import {useState} from 'react';
 
 function MyVerticallyCenteredModal(props) {
+    const [tripCreateData,setTripCreateData] = useState({
+        formGridStartDate : "2022-05-09",
+        formGridEndDate : "2022-05-20",
+        formGridLocation : "Singapore",
+        formGridActivityTitle : "Running",
+        formGridBriefDescription : "Fast and Far",
+        formGridThumbnailUrl : "www.google.com",
+        formGridCheckbox : true
+    }); 
+    
+    // handle change event
+    const handleChange = (e) => {
+        e.preventDefault(); // prevent the default action
+        setTripCreateData({...tripCreateData, [e.target.id]:e.target.value}); // set name to e.target.value (event)
+        console.log(tripCreateData); 
+    };
+
+    //post fetch call to create data in database, on submit
+
     return (
       <Modal
         {...props}
@@ -19,12 +39,22 @@ function MyVerticallyCenteredModal(props) {
             <Row className="mb-3">
                 <Form.Group as={Col} controlId="formGridStartDate">
                 <Form.Label>Start Date</Form.Label>
-                <Form.Control type="date" placeholder="Pick Start Date" />
+                <Form.Control 
+                    type="date" 
+                    placeholder="Pick Start Date" 
+                    value={tripCreateData.formGridStartDate} 
+                    onChange={handleChange}
+                />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridEndDate">
                 <Form.Label>End Date</Form.Label>
-                <Form.Control type="date" placeholder="Pick End Date" />
+                <Form.Control 
+                    type="date" 
+                    placeholder="Pick End Date" 
+                    value={tripCreateData.formGridEndDate} 
+                    onChange={handleChange}
+                />
                 </Form.Group>
             </Row>
 
@@ -38,19 +68,37 @@ function MyVerticallyCenteredModal(props) {
 
             <Form.Group className="mb-3" controlId="formGridActivityTitle">
                 <Form.Label>Activity Title</Form.Label>
-                <Form.Control placeholder="Brief title of trip" />
+                <Form.Control 
+                    type="text" 
+                    placeholder="Brief title of trip" 
+                    value={tripCreateData.formGridActivityTitle} 
+                    onChange={handleChange}
+                    />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Group className="mb-3" controlId="formGridBriefDescription">
                 <Form.Label>Brief Description of To Dos</Form.Label>
-                <Form.Control as="textarea" rows={3} />
+                <Form.Control 
+                    as="textarea" 
+                    rows={3} 
+                    value={tripCreateData.formGridBriefDescription} 
+                    onChange={handleChange}
+                />
             </Form.Group>
             <Row className="mb-3">
-                <Form.Group as={Col} controlId="formGridCity">
+                <Form.Group as={Col} controlId="formGridThumbnailUrl">
                 <Form.Label>Thumbnail Url</Form.Label>
-                <Form.Control/>
+                <Form.Control
+                    value={tripCreateData.formGridThumbnailUrl} 
+                    onChange={handleChange}
+                />
                 </Form.Group>
                 <Form.Group as={Col} className="mb-3" id="formGridCheckbox">
-                <Form.Check type="switch" label="Public Post" />
+                <Form.Check 
+                    type="switch" 
+                    label="Public Post" 
+                    value={tripCreateData.formGridCheckbox} // true false not working
+                    onChange={handleChange}
+                />
                 </Form.Group>
             </Row>
             <Button variant="primary" type="submit">
