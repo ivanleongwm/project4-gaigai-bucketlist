af89168777edc295f1e5214729a340d635632bc2
ivanleongwm <ivanleongwm@gmail.com>
2022-06-03 22:57:50 +0800

    added logged out route

--- frontend/src/components/HeaderBar/HeaderBar.jsx
+++ frontend/src/components/HeaderBar/HeaderBar.jsx
@@ -8,24 +8,29 @@ function HeaderContainer({loggedInUser}) {
         <div className="header-container">
             <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand as={Link} to="/">GaiGai</Navbar.Brand>
+                {
+                    loggedInUser ? 
+                    <Navbar.Brand as={Link} to="/">{'GaiGai  |  ' + loggedInUser}</Navbar.Brand>
+                    : <Navbar.Brand as={Link} to="/">GaiGai</Navbar.Brand>
+                }
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
                 </Nav>
                 <Nav>
-                    <Nav.Link >{loggedInUser ? loggedInUser : "Please Login"}</Nav.Link>
-                    <Nav.Link as={Link} to="people" >Trips</Nav.Link>
-                    <Nav.Link as={Link} to="login" >Profile</Nav.Link>
+                    {
+                        loggedInUser ? 
+                        <>
+                        <Nav.Link >Log Out</Nav.Link>
+                        <Nav.Link as={Link} to="/" >Trips</Nav.Link>
+                        <Nav.Link as={Link} to="profile" >Profile</Nav.Link>
+                        </> 
+                        :
+                        <>
+                            <Nav.Link as={Link} to="login">Login</Nav.Link>
+                            <Nav.Link as={Link} to="signup">Sign Up</Nav.Link>
+                        </>
+                    }
                 </Nav>
                 </Navbar.Collapse>
             </Container>
@@ -35,3 +40,15 @@ function HeaderContainer({loggedInUser}) {
 }
 
 export default HeaderContainer;
+
+/*
+<Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+*/
